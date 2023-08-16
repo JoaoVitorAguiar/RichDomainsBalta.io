a9d18ab8d611b14453dce2d3617c8d19a25c570a
jvAguiarSilva <107854662+jvAguiarSilva@users.noreply.github.com>
2023-08-15 21:01:45 -0300

    feat: test commands

---
+++ PaymentContext/PaymentContext.Tests/Commands/CreateBoletoSubscriptionCommandTests.cs
@@ -0,0 +1,21 @@
+﻿using PaymentContext.Domain.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PaymentContext.Tests.Commands;
+
+[TestClass]
+public class CreateBoletoSubscriptionCommandTests
+{
+    [TestMethod]
+    public void ShouldReturnErrorWhenNameIsInvalid()
+    {
+        var command = new CreateBoletoSubscriptionCommand();
+        command.FirstName = "";
+
+        command.Validate();
+        Assert.IsTrue(command.Invalid);
+    }
+}
